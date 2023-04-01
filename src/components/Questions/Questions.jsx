12a19acceb3933a939c94e21ef022898ba3619c1
shaheer-shamsi <115921517+shaheer-shamsi@users.noreply.github.com>
2023-04-01 23:31:26 +0600

    Update Questions.jsx

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -6,16 +6,16 @@ const Questions = () => {
                   <div className='text-xl font-semibold py-10 border p-5 space-y-2'>
         <h1 className='text-2xl pb-3 text-slate-800'>Questions?</h1>
       <details className='space-y-2'>
-        <summary>Props and State difference?</summary>
-        <p className='text-slate-700'>The props and state difference is props is immutable and it's read only. State changes can be asynchronous. And state can be modified.</p>
+        <summary>What is the difference between Props and State?</summary>
+        <p className='text-slate-700'>The main difference between props and state is that props is immutable and read only. State changes can be asynchronous. State can be modified.</p>
       </details>
       <details>
-        <summary>How does use state works?</summary>
-        <p className='text-slate-700'>Use state is a hook in react. It's allow us to add state functional components. It returns an array with two values 1. The current state and function to update it.</p>
+        <summary>How does useState work?</summary>
+        <p className='text-slate-700'>UseState is a hook in react. It allow us to add state functional components. It returns an array with two values 1. The current state and function to update it.</p>
       </details>
       <details>
-        <summary>What does use effect do in react?</summary>
-        <p className='text-slate-700'>Use effect fetch data from Api , setting up event listener , manipulating the DOM </p>
+        <summary>What does useEffect do in react?</summary>
+        <p className='text-slate-700'>UseEffect fetch data from Api , setting up event listener , manipulating the DOM </p>
       </details>
       <details>
         <summary>How does react work?</summary>
