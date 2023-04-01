5777e20a069bd77e576cda10d8bdb70fa71aefc2
shaheer-shamsi <115921517+shaheer-shamsi@users.noreply.github.com>
2023-04-01 23:35:11 +0600

    Update SideCart.jsx

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -4,11 +4,11 @@ const SideCart = ({ count, book }) => {
   return (
     <div className="sticky top-0">
       <div className="p-5 w-80 lg:w-96  border-violet-600 border rounded-lg font-semibold text-xl text-violet-800 bg-violet-100">
-        <h1>Spent time on read: {count}</h1>
+        <h1>Total time spent on read: {count}</h1>
       </div>
       <div className="bg-slate-200 w-80 bg-opacity-70 p-5 mt-5 rounded-lg lg:w-96 space-y-2">
         <h1 className="font-semibold text-slate-900 text-lg">
-          Bookmarked Blogs: {book.length}
+          Number of Bookmarked Blogs: {book.length}
         </h1>
         {book.map((item) => (
           <h1 className="text-lg bg-white p-3 rounded-lg font-semibold text-slate-800">
